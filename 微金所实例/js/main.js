e7d7a92117f84dd3ac7d73d1a83442efc505e323
welkin <welkin.qing@foxmail.com>
2018-09-13 22:43:54 +0800

    微金所实例

--- 微金所实例/js/main.js
+++ 微金所实例/js/main.js
@@ -24,4 +24,47 @@ $(function(){
     //不用手动触发===$(window).trigger('ressize')
     //让window对象立即触发
     $(window).on('resize',resize).trigger('resize');
+
+    //初始化tooltips插件
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+    })
+
+    //横向滚动条
+    var $ulContainer = $('.nav-tabs');
+    var width =30;
+    $ulContainer.children().each(function( index, element){
+        width += element.clientWidth
+    });
+    if(width> $(window).width()){
+        $ulContainer.css('width',width).parent().css('overflow-x','scroll');
+    }
+
+    //新闻列表切换主题
+    var $newsTitle = $('.news-title');
+    
+    $('#news .nav-pills a').on('click',function(){
+        var $this = $(this);
+        var title = $this.data('title');
+        $newsTitle.text(title);
+    });
+
+    //轮播图左滑右滑
+    var $carousel = $('.carousel');
+    var startX,endX;
+    var offset = 50;
+
+    $carousel.on(touchstart,function(e){
+        startX = e.originalEvent.touches[0].clientX;
+    });
+    $carousel.on(touchmove,function(e){
+        endX = e.originalEvent.touches[0].clientX;
+    })
+    $carousel.on(touchend,function(e){
+        console.log(e);
+        var distance = Math.abs(startX - endX);
+        if(distance > offset){
+            $(this).carousel(startX > endX ? 'next' : 'prev');
+        }
+    })
 });
